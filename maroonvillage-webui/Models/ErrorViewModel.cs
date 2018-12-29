a3ac8f14a152bb1d90ee7f254dc8d6679e720bc0
maroonvillage <maroonyear0@gmail.com>
2018-12-28 21:42:09 -0800

    Second commit.  Includes actual files

---
+++ maroonvillage-webui/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace maroonvillage_webui.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
